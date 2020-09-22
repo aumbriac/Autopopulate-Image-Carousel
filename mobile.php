71557dfcbf63e4fb55ed9cc4f9fa4eb8b4593f61
Anthony <anthonyumbriac@gmail.com>
2020-09-22 07:15:05 -0700

    Update mobile.php

--- mobile.php
+++ mobile.php
@@ -9,35 +9,10 @@ $randomImage = $images[array_rand($images)];
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
-<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
-<style>
-
-body {
-  background: #fff;
-}
-
-.swiper-container {
-  height: 100%;
-}
-
-#estate {
-  position: absolute;
-  color: #000;
-  font-size: 3rem;
-  font-family: 'Times New Roman', serif;
-  z-index: 2;
-  left: 50%;
-  transform: translateX(-50%);
-  font-weight: 400;
-  bottom: .5rem;
-  background: rgba(255,255,255,.3);
-}
-
-</style>
 <body>
 <!-- Slider main container -->
 <div class="swiper-container">
-    <!-- Additional required wrapper -->
+    <!-- Required wrapper -->
     <div class="swiper-wrapper">
         <!-- Slides -->
         <?php foreach($images as $image) : ?>
@@ -50,6 +25,7 @@ body {
     </div>
 </div>
 </body>
+<script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
 <script>
     var mySwiper = new Swiper('.swiper-container', {
   direction: 'horizontal',
